cf9d960c3b7ef91dbc9cc3a6660a71d2e026efee
Terence Daquiz <terence.daquiz@novare.com.hk>
2019-08-15 15:36:35 +0800

    Unang tulak

---
+++ Test Suites/Main Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Main Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d0ea9035-418e-4160-be27-7299129e3f6e</testSuiteGuid>
+   <testCaseLink>
+      <guid>237e631b-b0e5-4e38-b130-4189bfd614ef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Basic Log-in Feature</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Test Events Screen.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Events Screen</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7483e5fa-cb14-471f-8f8e-ca239a2d5fc3</testSuiteGuid>
+</TestSuiteEntity>
